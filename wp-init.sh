1a4f0eb468fa109bb3903db6db25cf40c9931e0b
Matthias Böhm <mail@matthiasboehm.com>
2019-03-14 19:00:52 +0100

    Change API base url to point to API service Docker container name

--- wp-init.sh
+++ wp-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wp plugin activate coc
 wp option add options_coc_api_key secret
-wp option add options_coc_api_url http://host.docker.internal:1337
+wp option add options_coc_api_url http://coc-api:1337
 wp post create --post_type=page --post_title='Clock of Change Frontend' --guid='clock-of-change-frontend' --post_status='publish' --post_content='<!-- wp:paragraph --><p>[coc\shortcodes\shworld]</p><!-- /wp:paragraph --><!-- wp:paragraph --><p>[coc\shortcodes\shsign]</p><!-- /wp:paragraph --><!-- wp:paragraph --><p>[coc\shortcodes\shsignup]</p><!-- /wp:paragraph --><!-- wp:paragraph --><p>[coc\shortcodes\shuserwall] </p><!-- /wp:paragraph -->'
