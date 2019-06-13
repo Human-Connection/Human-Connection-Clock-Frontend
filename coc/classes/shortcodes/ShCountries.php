38f9eb3d8eb8b5622e0ca0cf4a97be2614d1ea25
Matthias Böhm <mail@matthiasboehm.com>
2019-06-13 17:43:12 +0200

    Show country names

--- coc/classes/shortcodes/ShCountries.php
+++ coc/classes/shortcodes/ShCountries.php
@@ -21,11 +21,18 @@ class ShCountries
     private $api;
 
     /**
+     * @var object|null
+     */
+    private $countryNames;
+
+    /**
      * @param $api
      */
     public function __construct($api)
     {
         $this->api = $api;
+
+        $this->countryNames = $this->loadCountryNames();
 
         add_shortcode(strtolower(__CLASS__), [$this, 'renderShortcode']);
     }
@@ -46,15 +53,16 @@ class ShCountries
 
         $html = '<div id="country-rankings">';
 
-        for ($i = 0; $i < $maxNumber; $i++) {
+        $i = 0;
+        foreach($countries as $country) {
 
-            $imageSource = ClockOfChange::$pluginAssetsUri . '/images/flags/' . $countries[$i]->country . '.png';
+            $imageSource = ClockOfChange::$pluginAssetsUri . '/images/flags/' . strtolower($country->country) . '.png';
 
-            $html .= '<div class="country-ranking-item">';
-            $html .= '<img class="country-flag" alt="country flag" src=' . $imageSource . '>';
+            $html .= '<div class="country-ranking-item ' . ($i >= self::MAX_COUNTRIES ? 'hidden' : '') . '">';
+            $html .= '<img class="country-flag" alt="' . $this->getCountryName($country->country) . '" title="' . $this->getCountryName($country->country) . '" src=' . $imageSource . '>';
             $html .= '<div class="country-counter">';
 
-            $number = $countries[$i]->number;
+            $number = $country->number;
             for ($digit = 9; $digit > 0; $digit--) {
                 if ($digit > strlen($number)) {
                     $html .= '<span class="digit inactive">0</span>';
@@ -65,13 +73,46 @@ class ShCountries
 
             $html .= '</div>';
             $html .= '</div>';
+
+            $i++;
         }
+
+        $html .= '<div class="country-rankings-load-more"><a  href="#" class="load-more-link">mehr laden <i class="fa fa-chevron-down" aria-hidden="true"></i></a></div>';
 
         $html .= '</div>';
 
         //@todo handle load more requests
 
-
         return html_entity_decode($html);
+    }
+
+    /**
+     * @return object|null
+     */
+    private function loadCountryNames()
+    {
+        $countryNamesFilePath = get_home_path() . 'wp-content/plugins/coc/assets/js/countries.json';
+
+        if (file_exists($countryNamesFilePath)) {
+            $countryNames = file_get_contents($countryNamesFilePath);
+            return json_decode($countryNames);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $shortcode
+     * @return string
+     */
+    private function getCountryName($shortcode)
+    {
+        $propertyName = strtoupper($shortcode);
+
+        if (is_object($this->countryNames) && property_exists($this->countryNames, $propertyName)) {
+            return $this->countryNames->$propertyName;
+        }
+
+        return '';
     }
 }
