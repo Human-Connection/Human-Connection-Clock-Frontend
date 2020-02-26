3feadd61a51712de7b72e5b07156a44d93f88626
Matthias Böhm <mail@matthiasboehm.com>
2020-02-26 18:09:47 +0100

    Add new human graphic assets & fix rotation

--- coc/assets/js/old/clockAnimation.js
+++ coc/assets/js/old/clockAnimation.js
@@ -67,7 +67,7 @@ jQuery(document).ready(function ($) {
                 tlClock.pause();
 
                 tablet = false;
-                radius = 210;
+                radius = 211;
                 createInitTL();
                 createRotationTL();
                 createAddPersonTL();
@@ -288,7 +288,7 @@ jQuery(document).ready(function ($) {
             }
 
 
-            tl.set($(this), {opacity: 1, rotation: 90 + rot}, 'start+=' + (0.05 * cnt + 3.3));
+            tl.set($(this), {opacity: 1, rotation: 85 + rot}, 'start+=' + (0.05 * cnt + 3.3));
             tl.fromTo($(this), 1, {x: 0, y: 0}, {x: x, y: y, ease: Power1.easeOut}, 'start+=' + (0.08 * cnt + 3.3));
 
 //            tl.set($(this), {zIndex: cnt}, 'startPeople+=' + (0.08 * cnt + 1));
