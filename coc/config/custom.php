e16771d11e1bb7379ac61bafc37e0de16a5d9611
Matthias Böhm <mail@matthiasboehm.com>
2020-04-18 21:12:18 +0200

    Create custom options file to add options without using advanced custom fields wordpress plugin - content needs to be changed on the server

---
+++ coc/config/custom.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright Copyright 2020 | Human Connection gemeinnützige GmbH - Alle Rechte vorbehalten.
+ * @author    Matthias Böhm <mail@matthiasboehm.com>
+ */
+
+return [
+    'coc_api_url' => '',
+    'coc_api_key' => '',
+];
